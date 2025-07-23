4d0c9d18da9698d3a49d6ecd871b4ef71aad4e90
PeterOsoo <posoo617@gmail.com>
2025-07-23 11:59:37 +0300

    Created BlogPost API endpoint to list all posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
 # Create your views here.
+
+class BlogPostListView(APIView):
+    def get(self, request):
+        posts = BlogPost.objects.all().order_by('-created_at')
+        serializer = BlogPostSerializer(posts, many=True)
+        return Response(serializer.data)
