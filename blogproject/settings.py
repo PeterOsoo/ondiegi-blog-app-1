f8bf6e731e246fef868182bec12afe822afa42a1
PeterOsoo <posoo617@gmail.com>
2025-07-29 16:06:09 +0300

    🔧 Configure DEBUG and ALLOWED_HOSTS for custom error handling

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@nsdxfej5oa2=v!x+ngvth%1p-c29*a=&a^tx_vy0cbw95(&jy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
