3e0c216c29aaf9ce438fa836ec8cc201c9629ab3
PeterOsoo <posoo617@gmail.com>
2025-09-15 20:40:20 +0300

    add Category model and integrate category support into BlogPost admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
+from .models import BlogPost, Category
 
-from .models import BlogPost
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}  # auto-fill slug from name
+
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at')
-    search_fields = ('title', 'author__username')
-    ordering = ('-created_at',)
-
+    list_display = ("title", "author", "category", "created_at")
+    search_fields = ("title", "author__username", "content")
+    ordering = ("-created_at",)
+    list_filter = ("category", "created_at")
+    prepopulated_fields = {"slug": ("title",)}
