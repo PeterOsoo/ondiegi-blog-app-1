1a23832ff4f3001e17a8a74eb6ba92bdeda15fd9
PeterOsoo <posoo617@gmail.com>
2025-09-15 20:35:28 +0300

    add category ForeignKey to BlogPost (nullable for existing posts)

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,31 @@ from django.contrib.auth.models import User
 from django.utils.text import slugify
 import math
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=120, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+    
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)  # new field
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    category = models.ForeignKey(
+        Category,
+        related_name="posts",
+        on_delete=models.SET_NULL,
+        null=True,   # old posts can stay empty
+        blank=True
+    )
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -30,12 +49,3 @@ class BlogPost(models.Model):
 
     
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=120, unique=True)
-
-    class Meta:
-        verbose_name_plural = "Categories"
-
-    def __str__(self):
-        return self.name
