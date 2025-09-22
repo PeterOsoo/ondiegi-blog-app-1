9a29a6ac6aff9f9b3e5429cc3439e8be2aafe7d5
PeterOsoo <posoo617@gmail.com>
2025-09-22 22:23:23 +0300

    add categories to urls

--- blog/html_urls.py
+++ blog/html_urls.py
@@ -5,19 +5,21 @@ from .views import (
     create_post_view,
     BlogPostDetailView,
     BlogPostUpdateView,
-    BlogPostDeleteView
+    BlogPostDeleteView,
+    CategoryListHTMLView,
+    CategoryPostsHTMLView,
 )
 
-
 urlpatterns = [
-    path('', blog_list_view, name='blog-posts'),  # 👉 now serves HTML
+    path('', blog_list_view, name='blog-posts'),  # main blog list
     path('create/', create_post_view, name='create-post'),
+
+    # Category pages
+    path('categories/', CategoryListHTMLView.as_view(), name='category-list'),
+    path('categories/<slug:slug>/', CategoryPostsHTMLView.as_view(), name='category-posts'),
+
+    # Post detail/edit/delete
     path('<slug:slug>/', BlogPostDetailView.as_view(), name='view-post'),
     path('<slug:slug>/edit/', BlogPostUpdateView.as_view(), name='edit-post'),
     path('<slug:slug>/delete/', BlogPostDeleteView.as_view(), name='delete-post'),
-
 ]
-
-
-
-
